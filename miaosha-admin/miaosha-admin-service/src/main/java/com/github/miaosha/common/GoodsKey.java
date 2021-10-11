b13d91663215ec0021d59d069fbaa46f50716659
audience7510 <audience7510@163.com>
2021-10-11 16:29:22 +0800

    SpringCloud Alibaba秒杀

---
+++ miaosha-admin/miaosha-admin-service/src/main/java/com/github/miaosha/common/GoodsKey.java
@@ -0,0 +1,18 @@
+package com.github.miaosha.common;
+
+public class GoodsKey {
+
+	/**
+	 * @Description 获取商品列表key
+	 */
+	public static String getGoodsList = "list";
+	/**
+	 * @Description 获取商品详情key
+	 */
+	public static String getGoodsDetail = "detail";
+	/**
+	 * @Description 获取秒杀商品库存key
+	 */
+	public static String getMiaoshaGoodsStock = "stock";
+
+}
--- miaosha-admin/miaosha-admin-service/src/main/java/com/github/miaosha/controller/GoodsController.java
+++ miaosha-admin/miaosha-admin-service/src/main/java/com/github/miaosha/controller/GoodsController.java
@@ -1,16 +1,25 @@
 package com.github.miaosha.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.github.miaosha.common.GoodsKey;
 import com.github.miaosha.common.Result;
 import com.github.miaosha.common.ResultGen;
+import com.github.miaosha.pojo.po.Goods;
 import com.github.miaosha.redis.RedisUtil;
 import com.github.miaosha.service.IGoodsService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @ClassName GoodsController
@@ -30,11 +39,19 @@ public class GoodsController {
     @Autowired
     private RedisUtil redisUtil;
 
-    @GetMapping("findAll")
-    @ApiOperation(value = "查询所有用户列表",notes = "查询所有用户列表，全查，列表返回")
-    public Result findAll() {
-        redisUtil.set("name","小贝");
-        return ResultGen.success();
+    @GetMapping("list")
+    @ApiOperation(value = "查询所有商品列表",notes = "查询所有商品列表，列表返回")
+    public Result list() {
+        String s = (String) redisUtil.get(GoodsKey.getGoodsList);
+        if (!StringUtils.isEmpty(s)){
+            List<Goods> list = JSON.parseObject(s, List.class);
+            return ResultGen.success(list);
+        }else {
+            List<Goods> list = goodsService.list();
+            String jsonString = JSON.toJSONString(list);
+            redisUtil.set(GoodsKey.getGoodsList,jsonString);
+            return ResultGen.success(list);
+        }
     }
 
 }
